4df61ecc88953427e9f3bc58571d302310b65534
Elena Gerasimovich <nararock@mail.ru>
2023-11-28 00:18:37 +0300

    some fixed

--- task.py
+++ task.py
@@ -5,17 +5,18 @@ def find_corr(x_in, y_in):
     avg_x = sum(x_in) / len(x_in)
     avg_y = sum(y_in) / len(y_in)
 
-    def find_mult1(a, b):
+    def find_numerator(a, b):
         return (a - avg_x) * (b - avg_y)
 
-    def find_mult2(a):
+    def find_std1(a):
         return (a - avg_x) ** 2
 
-    def find_mult3(b):
+    def find_std2(b):
         return (b - avg_y) ** 2
 
-    return sum(map(find_mult1, x_in, y_in)) / (sum(map(find_mult2, x_in)) *
-                                               sum(map(find_mult3, y_in)))**0.5
+    return sum(map(find_numerator, x_in, y_in)) / (sum(map(find_std1, x_in)) *
+                                                   sum(map(find_std2,
+                                                           y_in))) ** 0.5
 
 
 x = [2, 1, 3, 9, 11, 12, 6, 15]
@@ -24,5 +25,3 @@ answer = find_corr(x, y)
 print(answer)
 # проверка
 print(pearsonr(x, y))
-
-
